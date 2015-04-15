2baea55e30407a69b6edc4d398e52781543a2640
Syairah <syairah@Nurs-MacBook-Pro.local>
2015-04-15 02:18:12 -0500

    cross tabs with lastvalue_dif

--- 01 SQL Crosstabs/syairah_crosstabs.R
+++ 01 SQL Crosstabs/syairah_crosstabs.R
@@ -55,14 +55,15 @@ tbl_df(cumedist)
 
 
 #determining the airtime  uniquecarriers relative to the max_airtime for a certain dist
+#tableau - sheet 14
 lastval_dif <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"select dest, uniquecarrier, airtime, last_value(max_airtime)
+"select distinct dest, uniquecarrier, airtime, last_value(max_airtime)
 OVER (PARTITION BY dest) AS max_airtime, last_value(max_airtime)
 OVER (PARTITION BY dest, uniquecarrier) - airtime  AS aberration
 FROM
 (SELECT dest, uniquecarrier, airtime, max(airtime)
-OVER (PARTITION BY dest, uniquecarrier) AS max_airtime
-FROM FT where origin = \\\'JFK\\\' AND cancelled = 0 AND (sdelay = 0 OR sdelay IS NULL) AND (weatherdelay = 0 OR weatherdelay IS NULL ) AND (securitydelay = 0 OR securitydelay IS NULL)) "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(lastval_dif)
+OVER (PARTITION BY dest) AS max_airtime
+FROM FT where origin = \\\'JFK\\\' AND cancelled = 0 AND (sdelay = 0 OR sdelay IS NULL) AND (weatherdelay = 0 OR weatherdelay IS NULL ) AND (securitydelay = 0 OR securitydelay IS NULL)) ORDER BY dest, uniquecarrier, airtime "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(lastval_dif)
 
 
 #ps : will try more for difference, couldn't find anything interesting - since we can't do SUM() and only difference .. Let me know if you have any idea
