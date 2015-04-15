5af53ebee9f59c96215b890024bf8418aca1dfc9
Syairah <syairah@wireless-10-145-70-162.public.utexas.edu>
2015-04-14 19:38:00 -0500

    haven't checked the last cross tab yet - server is down. Will run tonight and fix it if doesn't work -SR

--- 01 SQL Crosstabs/syairah_crosstabs.R
+++ 01 SQL Crosstabs/syairah_crosstabs.R
@@ -1,5 +1,8 @@
 require('RCurl')
 require("jsonlite")
+require('dplyr')
+require('plyr')
+require('tidyr')
 
 #rank top_destinations for each uniquecarrier
 rank <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query="
@@ -19,16 +22,14 @@ tbl_df(last_value)
 
 
 #query works in sql but not in r - CHECK
-#max_value - carriers with most delays. Can't do rank and pull total number of records for each 
+# max_value - carriers with most delays. Can't do rank and pull total number of records for each 
 max_value <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query="
-SELECT t.uniquecarrier, SUM(t.Avg_Arrdelay + t.Avg_Depdelay) AS TOTAL_DELAY
-FROM 
-(SELECT uniquecarrier, AVG(Arrdelay) AS Avg_Arrdelay, AVG(Depdelay) AS Avg_Depdelay 
+SELECT uniquecarrier, MAX(Arrdelay) AS Avg_Arrdelay, AVG(Depdelay) AS Avg_Depdelay 
 FROM FT 
-WHERE origin = \\\'JFK\\\' GROUP BY uniquecarrier) t
-GROUP BY t.uniquecarrier
-ORDER BY TOTAL_DELAY DESC"')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
+WHERE origin = \\\'JFK\\\' 
+GROUP BY uniquecarrier "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
 tbl_df(max_value)
+
 
 
 #2nd highest cancellation for each month
@@ -49,24 +50,20 @@ from ( Select dest, AVG(airtime) AS avg_airtime FROM FT where origin = \\\'JFK\\
 order by 2 "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
 tbl_df(cumedist)
 
-
-
-
-
-time_efficiency <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query="
-SELECT dest, uniquecarrier, min_airtime, max_airtime, Diff_airtime, rank() 
-OVER (PARTITION BY dest, uniquecarrier order by Diff_airtime desc) as AirTimeDuration_Rank 
-FROM (
-SELECT distinct dest, uniquecarrier, 
+time_efficiency <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "SELECT dest, uniquecarrier, min_airtime, max_airtime, Diff_airtime, rank() 
+OVER (PARTITION BY dest, uniquecarrier order by Diff_airtime desc) as AirTimeDuration_Rank
+FROM (SELECT dest, uniquecarrier, 
 first_value(airtime) IGNORE NULLS OVER (PARTITION BY dest, uniquecarrier ORDER BY airtime) AS min_airtime,
-last_value(airtime) IGNORE NULLS OVER (PARTITION BY dest, uniquecarrier ORDER BY airtime) AS max_airtime,,
+last_value(airtime) IGNORE NULLS OVER (PARTITION BY dest, uniquecarrier ORDER BY airtime) AS max_airtime,
 last_value(airtime) IGNORE NULLS OVER (PARTITION BY dest, uniquecarrier ORDER BY airtime) -
 first_value(airtime) IGNORE NULLS OVER (PARTITION BY dest, uniquecarrier ORDER BY airtime) AS Diff_airtime 
 FROM ( 
-SELECT dest, uniquecarrier, airtime FROM FT WHERE origin = \\\'JFK\\\' AND (cancelled = 0 0R cancelled IS NULL)
-AND (diverted = 0 OR diverted IS NULL ))
+SELECT dest, uniquecarrier, airtime FROM FT WHERE origin = \\\'JFK\\\' AND cancelled = 0 )
 ORDER BY DEST, uniquecarrier)
 WHERE AirTimeDuration_Rank = 1
 "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
 tbl_df(time_efficiency)
+
+
+
 
