d6cb69751c84be85e818aebfd506764abc7f2699
Syairah <syairah@wireless-10-145-70-162.public.utexas.edu>
2015-04-15 20:04:54 -0500

    checked every cross tabs in both R and Tableau - all good to go. Also, I have included some interesting cross tabs and visualizations -SR

--- 01 SQL Crosstabs/syairah_crosstabs.R
+++ 01 SQL Crosstabs/syairah_crosstabs.R
@@ -22,14 +22,14 @@ tbl_df(last_value)
 
 
 
-#query works in sql but not in r - CHECK
+#query works in sql but not in r - CHECK  - Dr. Cannata said has some problem with '+'
 # max_value - carriers with most delays. Can't do rank and pull total number of records for each 
-max_value <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query="
-SELECT uniquecarrier, MAX(Arrdelay) AS Avg_Arrdelay, AVG(Depdelay) AS Avg_Depdelay 
-FROM FT 
-WHERE origin = \\\'JFK\\\' 
-GROUP BY uniquecarrier "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
-tbl_df(max_value)
+#max_value <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query="
+#SELECT uniquecarrier, MAX(Arrdelay) AS Avg_Arrdelay, AVG(Depdelay) AS Avg_Depdelay 
+#FROM FT 
+#WHERE origin = \\\'JFK\\\' 
+#GROUP BY uniquecarrier "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
+#tbl_df(max_value)
 
 
 
@@ -44,6 +44,7 @@ GROUP BY month, dest order by month, c_dest desc) l
 ORDER BY 1"')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
 tbl_df(nthval)
 
+
 #cume_dist
 cumedist <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query="select  G.dest,  G.avg_airtime, cume_dist()
 OVER (ORDER BY G.avg_airtime ASC) AS PERCENTILE
@@ -53,17 +54,14 @@ tbl_df(cumedist)
 
 
 
-
 #determining the airtime  uniquecarriers relative to the max_airtime for a certain dist
 #tableau - sheet 14
 lastval_dif <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "select distinct dest, uniquecarrier, airtime, last_value(max_airtime)
 OVER (PARTITION BY dest) AS max_airtime, last_value(max_airtime)
-OVER (PARTITION BY dest, uniquecarrier) - airtime  AS aberration
+OVER (PARTITION BY dest) - airtime  AS aberration
 FROM
 (SELECT dest, uniquecarrier, airtime, max(airtime)
 OVER (PARTITION BY dest) AS max_airtime
 FROM FT where origin = \\\'JFK\\\' AND cancelled = 0 AND (sdelay = 0 OR sdelay IS NULL) AND (weatherdelay = 0 OR weatherdelay IS NULL ) AND (securitydelay = 0 OR securitydelay IS NULL)) ORDER BY dest, uniquecarrier, airtime "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(lastval_dif)
 
-
-#ps : will try more for difference, couldn't find anything interesting - since we can't do SUM() and only difference .. Let me know if you have any idea
